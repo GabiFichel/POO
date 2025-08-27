8d9f5b5066e94283880eae2e657e7de2628c7fad
Gabriela Fichel <mariagabrielasantos1a@gmail.com>
2025-08-27 10:11:36 -0300

    ENUM 25/08

---
+++ ENUM/DiasSemanas.java
@@ -0,0 +1,11 @@
+package ENUM;
+
+public enum DiasSemanas {
+    DOMINGO,
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO,
+}
---
+++ ENUM/EnumDias.java
@@ -0,0 +1,9 @@
+package ENUM;
+
+public class EnumDias {
+    public static void main(String[] args){
+        DiasSemanas dia = DiasSemanas.SEGUNDA;
+
+        System.out.println(dia);
+    }
+}
